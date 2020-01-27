db78ad6a1779a54f361a19e7d7a7f4758117f2c3
MunaLugonvu <munalugonvu@gmail.com>
2020-01-27 20:47:01 +0300

    update

--- Intern/routes/interventions.js
+++ Intern/routes/interventions.js
@@ -45,11 +45,45 @@ router.post('/', async(req, res) => {
 
 
 // Update one intervention
-router.patch('/:id', (req, res) => {
+router.patch('/:id',getIntervention, async(req, res) => {
+    if (req.body.createdBy != null) {
+        res.intervention.createdBy= req.body.createdBy
+      }
+    
+      if (req.body.tittle != null) {
+        res.intervention.tittle= req.body.tittle
+      }
+      if (req.body.type!= null) {
+        res.intervention.type= req.body.type
+      }
+      if (req.body.location != null) {
+        res.intervention.location= req.body.location
+      }
+      if (req.body.status != null) {
+        res.intervention.status= req.body.status
+      }
+
+      if (req.body.comment != null) {
+        res.intervention.comment= req.body.comment
+      }
+
+      try {
+        let updatedIntervention = await res.intervention.save()
+        res.json(updatedIntervention)
+      } catch {
+        res.status(400).json({ message: err.message })
+      }
+    
 })
 
 // Delete one intervention
-router.delete('/:id', (req, res) => {
+router.delete('/:id', getIntervention,async(req, res) => {
+    try {
+        await res.intervention.remove()
+        res.json({ message: 'Deleted This Intervention' })
+      } catch(err) {
+        res.status(500).json({ message: err.message })
+      }
 })
 
 async function getIntervention(req, res, next) {
