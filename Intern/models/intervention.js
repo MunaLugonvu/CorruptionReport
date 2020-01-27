ff21239d07e9e4885c911c67451cdf0ce97b1df8
MunaLugonvu <munalugonvu@gmail.com>
2020-01-27 17:11:32 +0300

    Update

--- Intern/models/intervention.js
+++ Intern/models/intervention.js
@@ -1,15 +1,50 @@
 let mongoose = require('mongoose')
 let interventionSchema = new mongoose.Schema({
-    id:{
-        type: integer,
-        required: true
-    },
+    
+    
     createdOn:{
         type: Date,
         required: true,
         default: Date.now
 
     },
-    
+    createdBy:{
+        type: Number,
+        required: true
+
+    },
+    tittle:{
+        type: String,
+        required: true
+
+    },
+    type:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        required: true
+
+    },
+    images:{
+        type: Buffer,
+        required: false
+    },
+    videos:{
+        type: Buffer,
+        required: false
+
+    },
+    comment:{
+        type: String,
+        required: true
+    }
+
 
 })
+module.exports = mongoose.model('Intervention', interventionSchema)
--- Intern/routes/interventions.js
+++ Intern/routes/interventions.js
@@ -1,20 +1,48 @@
 let express = require('express')
 let router = express.Router()
+let Intervention = require('../models/intervention')
+
 
 
 
 // Get all interventions
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async(req, res) => {
+    try {
+        let interventions = await Intervention.find()
+        res.json(interventions)
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
 });
 
 // Get one intervention
-router.get('/:id', (req, res) => {
+router.get('/:id',getIntervention, (req, res) => {
+   
+    
+    res.json(res.intervention)
 })
 
 // Create one intervention
-router.post('/', (req, res) => {
-})
+router.post('/', async(req, res) => {
+    let intervention = new Intervention({
+        
+        createdOn:req.body.createdOn,
+        createdBy: req.body.createdBy,
+        tittle:req.body.tittle,
+        type:req.body.type,
+        location:req.body.location,
+        status:req.body.status,
+        comment:req.body.comment
+      })
+    
+      try {
+        const newIntervention = await intervention.save()
+        res.status(201).json(newIntervention)
+      } catch (err) {
+        res.status(400).json({ message: err.message })
+      }
+    });
+
 
 // Update one intervention
 router.patch('/:id', (req, res) => {
@@ -24,10 +52,19 @@ router.patch('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
 })
 
-
-
-
-
+async function getIntervention(req, res, next) {
+    try {
+      intervention = await Intervention.findById(req.params.id)
+      if (intervention == null) {
+        return res.status(404).json({ message: 'Cant find Intervention'})
+      }
+    } catch(err){
+      return res.status(500).json({ message: err.message })
+    }
+  
+    res.intervention = intervention;
+    next()
+  }
 
 
 
@@ -90,3 +127,4 @@ router.delete('/:id', (req, res) => {
 
 
 module.exports = router
+
